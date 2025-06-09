2fabf465dc42ab7518ca584af0cb11e9c3f0e7b7
Dr. Amr Osman <dr3mro@gmail.com>
2025-06-08 22:17:15 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/react-todo-app/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt"],
+      manifest: {
+        name: "Shopping List",
+        short_name: "App",
+        start_url: "/react-todo-app",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "assets/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
