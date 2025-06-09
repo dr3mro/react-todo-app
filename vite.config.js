c7505df0189483ec6525f79619770b9a6aad6669
Dr. Amr Osman <dr3mro@gmail.com>
2025-06-08 22:23:44 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -24,12 +24,12 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "assets/icon-192.png",
+            src: "/react-todo-app/assets/icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "assets/icon-512.png",
+            src: "/react-todo-app/assets/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
